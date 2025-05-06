5b5f44219b445750ef74bec150ac26de47b57c14
Mike Alhayek <mike@crestapps.com>
2025-05-06 08:25:50 -0700

    use notification templates

--- src/PureCloud.Client/Extensions/Notifications/SubscriptionBuilder.cs
+++ src/PureCloud.Client/Extensions/Notifications/SubscriptionBuilder.cs
@@ -14,19 +14,28 @@ public sealed class SubscriptionBuilder
         return this;
     }
 
+    /// <summary>
+    /// Register the event type corresponding to the given topic template.
+    /// </summary>
+    /// <param name="topicTemplate"></param>
+    /// <param name="ids"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
     public SubscriptionBuilder Add(string topicTemplate, params string[] ids)
     {
-        if (NotificationTopics.Topics.TryGetValue(topicTemplate, out var topicType))
+        if (!PureCloudConstants.TopicTemplates.TryGetValue(topicTemplate, out var topicType))
         {
-            var topic = topicTemplate;
-
-            if (ids is not null && ids.Length > 0)
-            {
-                topic = string.Format(topicTemplate, ids);
-            }
-
-            _map[topic] = topicType;
+            throw new InvalidOperationException($"The topic template '{topicTemplate}' is invalid. Valid templates can be found in {nameof(PureCloudConstants)}.{nameof(PureCloudConstants.TopicTemplates)}");
         }
+
+        var topic = topicTemplate;
+
+        if (ids is not null && ids.Length > 0)
+        {
+            topic = string.Format(topicTemplate, ids);
+        }
+
+        _map[topic] = topicType;
 
         return this;
     }
--- src/PureCloud.Client/Models/ConversationCallEventTopicCallConversation.cs
+++ src/PureCloud.Client/Models/ConversationCallEventTopicCallConversation.cs
@@ -1,63 +1,51 @@
-using System.Text;
-using System.Text.Json.Serialization;
-
 namespace PureCloud.Client.Models;
 
-public sealed partial class ConversationCallEventTopicCallConversation
+public sealed class ConversationCallEventTopicCallConversation
 {
     /// <summary>
     /// Gets or Sets RecordingState
     /// </summary>
-    public RecordingStateEnum? RecordingState { get; set; }
-
+    public ConversationCallEventTopicCallConversationRecordingState? RecordingState { get; set; }
 
     /// <summary>
     /// Gets or Sets Id
     /// </summary>
     public string Id { get; set; }
 
-
     /// <summary>
     /// Gets or Sets Name
     /// </summary>
     public string Name { get; set; }
-
 
     /// <summary>
     /// Gets or Sets Participants
     /// </summary>
     public IEnumerable<ConversationCallEventTopicCallMediaParticipant> Participants { get; set; }
 
-
     /// <summary>
     /// Gets or Sets OtherMediaUris
     /// </summary>
     public IEnumerable<string> OtherMediaUris { get; set; }
-
 
     /// <summary>
     /// Gets or Sets Address
     /// </summary>
     public string Address { get; set; }
 
-
     /// <summary>
     /// Gets or Sets UtilizationLabelId
     /// </summary>
     public string UtilizationLabelId { get; set; }
-
 
     /// <summary>
     /// Gets or Sets Divisions
     /// </summary>
     public IEnumerable<ConversationCallEventTopicConversationDivisionMembership> Divisions { get; set; }
 
-
     /// <summary>
     /// Gets or Sets SecurePause
     /// </summary>
     public bool? SecurePause { get; set; }
-
 
     /// <summary>
     /// Gets or Sets MaxParticipants
