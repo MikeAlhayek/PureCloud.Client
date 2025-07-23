239f878f032f065a9a7079973e66309d5b5fedf7
jack Telford <jtelford@rdicorp.com>
2025-07-23 11:06:06 -0400

    Format Models OrganizationApi

--- src/PureCloud.Client/Models/OrgAuthSettings.cs
+++ src/PureCloud.Client/Models/OrgAuthSettings.cs
@@ -1,37 +1,11 @@
 namespace PureCloud.Client.Models;
 
-/// <summary>
-/// OrgAuthSettings
-/// </summary>
 public sealed class OrgAuthSettings
 {
-    /// <summary>
-    /// Indicates whether multi-factor authentication is required.
-    /// </summary>
     public bool? MultifactorAuthenticationRequired { get; set; }
-
-    /// <summary>
-    /// Indicates whether the domain allowlist is enabled.
-    /// </summary>
     public bool? DomainAllowlistEnabled { get; set; }
-
-    /// <summary>
-    /// The list of domains that will be allowed to embed Genesys Cloud applications.
-    /// </summary>
-    public List<string> DomainAllowlist { get; set; }
-
-    /// <summary>
-    /// The list of IP addresses that will be allowed to authenticate with Genesys Cloud.
-    /// </summary>
-    public List<string> IpAddressAllowlist { get; set; }
-
-    /// <summary>
-    /// The password requirements for the organization.
-    /// </summary>
+    public IEnumerable<string> DomainAllowlist { get; set; }
+    public IEnumerable<string> IpAddressAllowlist { get; set; }
     public PasswordRequirements PasswordRequirements { get; set; }
-
-    /// <summary>
-    /// The list of exempt apis from inactivity timeout.
-    /// </summary>
-    public List<string> InactivityTimeoutExclusions { get; set; }
+    public IEnumerable<string> InactivityTimeoutExclusions { get; set; }
 }
--- src/PureCloud.Client/Models/Organizations/Organization.cs
+++ src/PureCloud.Client/Models/Organizations/Organization.cs
@@ -2,89 +2,19 @@ namespace PureCloud.Client.Models.Organizations;
 
 public sealed class Organization
 {
-
-    /// <summary>
-    /// The current state. Examples are active, inactive, deleted.
-    /// </summary>
-    /// <value>The current state. Examples are active, inactive, deleted.</value>
     public OrganizationState? State { get; set; }
-
-    /// <summary>
-    /// Organizations Originating Platform.
-    /// </summary>
-    /// <value>Organizations Originating Platform.</value>
     public ProductPlatform? ProductPlatform { get; set; }
-
-    /// <summary>
-    /// The globally unique identifier for the object.
-    /// </summary>
-    /// <value>The globally unique identifier for the object.</value>
     public string Id { get; set; }
-
-    /// <summary>
-    /// Gets or Sets Name
-    /// </summary>
     public string Name { get; set; }
-
-    /// <summary>
-    /// The default language for this organization. Example: &#39;en&#39;
-    /// </summary>
-    /// <value>The default language for this organization. Example: &#39;en&#39;</value>
     public string DefaultLanguage { get; set; }
-
-    /// <summary>
-    /// The default country code for this organization. Example: &#39;US&#39;
-    /// </summary>
-    /// <value>The default country code for this organization. Example: &#39;US&#39;</value>
     public string DefaultCountryCode { get; set; }
-
-    /// <summary>
-    /// The short name for the organization. This field is globally unique and cannot be changed.
-    /// </summary>
-    /// <value>The short name for the organization. This field is globally unique and cannot be changed.</value>
     public string ThirdPartyOrgName { get; set; }
-
-    /// <summary>
-    /// Gets or Sets ThirdPartyURI
-    /// </summary>
     public string ThirdPartyURI { get; set; }
-
-    /// <summary>
-    /// Gets or Sets Domain
-    /// </summary>
     public string Domain { get; set; }
-
-    /// <summary>
-    /// The current version of the organization.
-    /// </summary>
-    /// <value>The current version of the organization.</value>
     public int? Version { get; set; }
-
-    /// <summary>
-    /// Gets or Sets DefaultSiteId
-    /// </summary>
     public string DefaultSiteId { get; set; }
-
-    /// <summary>
-    /// Email address where support tickets are sent to.
-    /// </summary>
-    /// <value>Email address where support tickets are sent to.</value>
     public string SupportURI { get; set; }
-
-    /// <summary>
-    /// Gets or Sets VoicemailEnabled
-    /// </summary>
     public bool? VoicemailEnabled { get; set; }
-
-    /// <summary>
-    /// The URI for this object
-    /// </summary>
-    /// <value>The URI for this object</value>
     public string SelfUri { get; set; }
-
-    /// <summary>
-    /// The state of features available for the organization.
-    /// </summary>
-    /// <value>The state of features available for the organization.</value>
     public Dictionary<string, bool?> Features { get; set; }
 }
